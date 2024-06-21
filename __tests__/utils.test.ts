095713a614fee499dba333219727837f7b7184cd
granitosaurus <bernardas.alisauskas@pm.me>
2024-06-21 21:39:14 +0700

    ensure all of + and / are replaced to - and _ not only the first occurances; closes issue #2

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -5,4 +5,7 @@ describe('urlsafe_b64encode', () => {
     it('should encode a string to base64', () => {
         expect(urlsafe_b64encode('hello+foo/bar=====')).toBe('aGVsbG8rZm9vL2Jhcj09PT09');
     });
+    it('multiple characters should be encoded correctly', () => {
+        // XXX: investigate how to produce a string that contains + or / when b64 encoded.
+    });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
 export function urlsafe_b64encode(data: string): string {
-    return Buffer.from(data, 'utf-8').toString('base64').replace('+', '-').replace('/', '_').replace(/=+$/, '');
+    return Buffer.from(data, 'utf-8').toString('base64')
+        .replace(/\+/g, '-')  // Replace all instances of '+' with '-'
+        .replace(/\//g, '_')  // Replace all instances of '/' with '_'
+        .replace(/=+$/, '');  // Remove trailing '=' characters
 }
